fee725ee9195f3a4bcc3c2cd3435d205cc65b8f9
Frederike Bründermann <82148810+frederikebruendermann@users.noreply.github.com>
2021-04-30 20:37:52 +0200

    Add navigation modules

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import getElement from './utils/getElement'
 import setupNavigation from './setupNavigation'
 
 const bookmark = getElement('.card__bookmark')
-const pAnswer = getElement('.card__answer')
-const buttonAnswer = getElement('.button__answer')
 const buttonDarkmode = getElement('.app-profile__darkmode')
 const body = getElement('.body_darkmode')
+const pAnswer = document.querySelector('.card__answer')
+const buttonAnswer = document.querySelector('.button__answer')
 
 setupNavigation()
 
@@ -13,10 +13,10 @@ bookmark.addEventListener('click', () => {
   bookmark.classList.toggle('card__bookmark--selected')
 })
 
-buttonAnswer.addEventListener('click', () => {
-  pAnswer.classList.toggle('hidden')
-})
-
 buttonDarkmode.addEventListener('click', () => {
   body.classList.toggle('darkmode')
+})
+
+buttonAnswer.addEventListener('click', () => {
+  pAnswer.classList.toggle('hidden')
 })
